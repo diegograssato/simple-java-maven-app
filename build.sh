b24271dad7afd90e387b253988a5e8d18d553c88
Diego Pereira Grassato <dgrassato@ciandt.com>
2019-01-02 15:05:16 +0000

    Fix color

--- build.sh
+++ build.sh
@@ -1,7 +1,29 @@
 #!/usr/bin/env bash
 
-  
+ 
+NONE='\e[0m' # Eliminar as Cores, deixar padrão)
 
-echo -e "Default \e[31mRed"
-echo -e "Default \e[36mCyan"
-echo -e "Default \e[91mLight red"
+## Cores de Fonte
+# Regular Colors
+BLACK='\e[1;30m'        # Black
+RED='\e[1;31m'          # Red
+GREEN='\e[1;32m'        # Green
+YELLOW='\e[1;33m'       # Yellow
+BLUE='\e[1;34m'         # Blue
+PURPLE='\e[1;35m'       # Purple
+CYAN='\e[1;36m'         # Cyan
+WHITE='\e[1;37m'        # White
+GRAY='\e[1;90m'         # Gray
+
+echo -e "${BLACK}[✓] BUILDING my project...${NONE}"
+echo -e "${RED}[✓] BUILDING my project...${NONE}"
+echo -e "${GREEN}[✓] BUILDING my project...${NONE}"
+echo -e "${YELLOW}[✓] BUILDING my project...${NONE}"
+echo -e "${BLUE}[✓] BUILDING my project...${NONE}"
+echo -e "${PURPLE}[✓] BUILDING my project...${NONE}"
+echo -e "${CYAN}[✓] BUILDING my project...${NONE}"
+echo -e "${WHITE}[✓] BUILDING my project...${NONE}"
+echo -e "${GRAY}[✓] BUILDING my project...${NONE}"
+
+
+ 
