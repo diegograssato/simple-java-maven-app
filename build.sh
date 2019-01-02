ae2b61b87000b6b09485d4704b069319b017b9b5
Diego Pereira Grassato <dgrassato@ciandt.com>
2019-01-02 16:26:49 +0000

    Fix color final

--- build.sh
+++ build.sh
@@ -3,35 +3,35 @@
 NONE='\e[0m\n' # Eliminar as Cores, deixar padrão)
 
 # Regular Colors
-BLACK='\e[30m'       # Black
-RED='\e[31m'         # Red
-GREEN='\e[32m'       # Green
-YELLOW='\e[33m'      # Yellow
-BLUE='\e[34m'        # Blue
-PURPLE='\e[35m'      # Purple
-CYAN='\e[36m'        # Cyan
-WHITE='\e[37m'       # White
+BLACK='\e[1;30m'       # Black
+RED='\e[1;31m'         # Red
+GREEN='\e[1;32m'       # Green
+YELLOW='\e[1;33m'      # Yellow
+BLUE='\e[1;34m'        # Blue
+PURPLE='\e[1;35m'      # Purple
+CYAN='\e[1;36m'        # Cyan
+WHITE='\e[1;37m'       # White
 
 
 #Danger/fail
-printf "\n${RED}[$(date +%H:%M:%S)][ ✘ ] Agora vai ${NONE}"
+printf "\n${RED}[$(date +%H:%M:%S)][ ✘ ] Agora vai!"
 
 #Sucess
-printf "\n${GREEN}[$(date +%H:%M:%S)][ ✔ ] Agora vai ${NONE}"
+printf "\n${GREEN}[$(date +%H:%M:%S)][ ✔ ] Agora vai!"
 
 # Warning
-printf "\n${YELLOW}[$(date +%H:%M:%S)][ ⚠ ] Agora vai ${NONE}"
+printf "\n${YELLOW}[$(date +%H:%M:%S)][ ⚠ ] Agora vai!"
 
 # Question
-printf "\n${CYAN}[$(date +%H:%M:%S)][ ? ] Agora vai ${NONE}"
+printf "\n${CYAN}[$(date +%H:%M:%S)][ ? ] Agora vai!"
 
 # Info
-printf "\n${BLUE}[$(date +%H:%M:%S)][ * ] Agora vai ${NONE}"
+printf "\n${BLUE}[$(date +%H:%M:%S)][ * ] Agora vai!"
 
 
-printf "\n${WHITE}[$(date +%H:%M:%S)][ * ] Agora vai ${NONE}"
-printf "\n${BLACK}[$(date +%H:%M:%S)][ * ] Agora vai ${NONE}"
-printf "\n${PURPLE}[$(date +%H:%M:%S)][ * ] Agora vai ${NONE}"
+printf "\n${WHITE}[$(date +%H:%M:%S)][ * ] Agora vai!"
+printf "\n${BLACK}[$(date +%H:%M:%S)][ * ] Agora vai!"
+printf "\n${PURPLE}[$(date +%H:%M:%S)][ * ] Agora vai!"
 printf "\n${PURPLE}[$(date +%H:%M:%S)]${BLACK}[ * ] Agora ${BLUE} vai"
 printf "${NONE}"
 echo
