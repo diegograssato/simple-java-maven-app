3179ae14df0d0e388bef6a1002f9e80111320c1a
Diego Pereira Grassato <dgrassato@ciandt.com>
2019-01-02 15:14:30 +0000

    Fix color

--- build.sh
+++ build.sh
@@ -15,15 +15,15 @@ CYAN='\e[36m'         # Cyan
 WHITE='\e[37m'        # White
 GRAY='\e[90m'         # Gray
 
-echo -e "${BLACK} [✓] BUILDING my project..."
-echo -e "${RED} [✓] BUILDING my project..."
-echo -e "${GREEN} [✓] BUILDING my project..."
-echo -e "${YELLOW} [✓] BUILDING my project..."
-echo -e "${BLUE} [✓] BUILDING my project..."
-echo -e "${PURPLE} [✓] BUILDING my project..."
-echo -e "${CYAN} [✓] BUILDING my project..."
-echo -e "${WHITE} [✓] BUILDING my project..."
-echo -e "${GRAY} [✓] BUILDING my project..."
+echo -e "${BLACK} BUILDING my project..."
+echo -e "${RED} BUILDING my project..."
+echo -e "${GREEN} BUILDING my project..."
+echo -e "${YELLOW} BUILDING my project..."
+echo -e "${BLUE} BUILDING my project..."
+echo -e "${PURPLE} BUILDING my project..."
+echo -e "${CYAN} BUILDING my project..."
+echo -e "${WHITE} BUILDING my project..."
+echo -e "${GRAY} BUILDING my project..."
 
 
  
