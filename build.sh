e0bbb6a3523c3d73555e673d06987c9236b040ce
Diego Pereira Grassato <dgrassato@ciandt.com>
2019-01-02 14:44:24 +0000

    Fix color

--- build.sh
+++ build.sh
@@ -15,38 +15,77 @@ NONE="\033[0m" # Eliminar as Cores, deixar padrão)
 set -o errexit # make your script exit when a command fails
 
 ## Cores de Fonte
-K="\[\033[0;30m\]" # Black (Preto)
-R="\[\033[0;31m\]" # Red (Vermelho)
-G="\[\033[0;32m\]" # Green (Verde)
-Y="\[\033[0;33m\]" # Yellow (Amarelo)
-B="\[\033[0;34m\]" # Blue (Azul)
-M="\[\033[0;35m\]" # Magenta (Vermelho Claro)
-C="\[\033[0;36m\]" # Cyan (Ciano - Azul Claro)
-W="\[\033[0;37m\]" # White (Branco)
+# Regular Colors
+BLACK='\033[0;30m'        # Black
+RED='\033[0;31m'          # Red
+GREEN='\033[0;32m'        # Green
+YELLOW='\033[0;33m'       # Yellow
+BLUE='\033[0;34m'         # Blue
+PURPLE='\033[0;35m'       # Purple
+CYAN='\033[0;36m'         # Cyan
+WHITE='\033[0;37m'        # White
 
-## Efeito Negrito (bold) e cores
-BK="\[\033[1;30m\]" # Bold+Black (Negrito+Preto)
-BR="\[\033[1;31m\]" # Bold+Red (Negrito+Vermelho)
-BG="\[\033[1;32m\]" # Bold+Green (Negrito+Verde)
-BY="\[\033[1;33m\]" # Bold+Yellow (Negrito+Amarelo)
-BB="\[\033[1;34m\]" # Bold+Blue (Negrito+Azul)
-BM="\[\033[1;35m\]" # Bold+Magenta (Negrito+Vermelho Claro)
-BC="\[\033[1;36m\]" # Bold+Cyan (Negrito+Ciano - Azul Claro)
-BW="\[\033[1;37m\]" # Bold+White (Negrito+Branco)
+# Bold
+BBLACK='\033[1;30m'       # Black
+BRED='\033[1;31m'         # Red
+BGREEN='\033[1;32m'       # Green
+BYELLOW='\033[1;33m'      # Yellow
+BBLUE='\033[1;34m'        # Blue
+BPURPLE='\033[1;35m'      # Purple
+BCYAN='\033[1;36m'        # Cyan
+BWHITE='\033[1;37m'       # White
 
-## Cores de fundo (backgroud)
-BGK="\[\033[40m\]" # Black (Preto)
-BGR="\[\033[41m\]" # Red (Vermelho)
-BGG="\[\033[42m\]" # Green (Verde)
-BGY="\[\033[43m\]" # Yellow (Amarelo)
-BGB="\[\033[44m\]" # Blue (Azul)
-BGM="\[\033[45m\]" # Magenta (Vermelho Claro)
-BGC="\[\033[46m\]" # Cyan (Ciano - Azul Claro)
-BGW="\[\033[47m\]" # White (Branco)
+# Underline
+UBLACK='\033[4;30m'       # Black
+URED='\033[4;31m'         # Red
+UGREEN='\033[4;32m'       # Green
+UYELLOW='\033[4;33m'      # Yellow
+UBLUE='\033[4;34m'        # Blue
+UPURPLE='\033[4;35m'      # Purple
+UCYAN='\033[4;36m'        # Cyan
+UWHITE='\033[4;37m'       # White
 
+# Background
+ON_BLACK='\033[40m'       # Black
+ON_RED='\033[41m'         # Red
+ON_GREEN='\033[42m'       # Green
+ON_YELLOW='\033[43m'      # Yellow
+ON_BLUE='\033[44m'        # Blue
+ON_PURPLE='\033[45m'      # Purple
+ON_CYAN='\033[46m'        # Cyan
+ON_WHITE='\033[47m'       # White
 
-echo -e "${BG}[✓] BUILDING my project...${NONE}"
-echo -e "${BR}[✓] BUILDING my project...${NONE}"
-echo -e "${BY}[✓] BUILDING my project...${NONE}"
-echo -e "${BC}[✓] BUILDING my project...${NONE}"
-echo -e "${BK}[✓] BUILDING my project...${NONE}"
+# High Intensity
+IBLACK='\033[0;90m'       # Black
+IRED='\033[0;91m'         # Red
+IGREEN='\033[0;92m'       # Green
+IYELLOW='\033[0;93m'      # Yellow
+IBLUE='\033[0;94m'        # Blue
+IPURPLE='\033[0;95m'      # Purple
+ICYAN='\033[0;96m'        # Cyan
+IWHITE='\033[0;97m'       # White
+
+# Bold High Intensity
+BIBLACK='\033[1;90m'      # Black
+BIRED='\033[1;91m'        # Red
+BIGREEN='\033[1;92m'      # Green
+BIYELLOW='\033[1;93m'     # Yellow
+BIBLUE='\033[1;94m'       # Blue
+BIPURPLE='\033[1;95m'     # Purple
+BICYAN='\033[1;96m'       # Cyan
+BIWHITE='\033[1;97m'      # White
+
+# High Intensity backgrounds
+ON_IBLACK='\033[0;100m'   # Black
+ON_IRED='\033[0;101m'     # Red
+ON_IGREEN='\033[0;102m'   # Green
+ON_IYELLOW='\033[0;103m'  # Yellow
+ON_IBLUE='\033[0;104m'    # Blue
+ON_IPURPLE='\033[0;105m'  # Purple
+ON_ICYAN='\033[0;106m'    # Cyan
+ON_IWHITE='\033[0;107m'   # White
+echo -e "${IRED}[✓] BUILDING my project...${NONE}"
+echo -e "${BIBLACK}[✓] BUILDING my project...${NONE}"
+echo -e "${BICYAN}[✓] BUILDING my project...${NONE}"
+echo -e "${ON_GREEN}[✓] BUILDING my project...${NONE}"
+echo -e "${IBLUE}[✓] BUILDING my project...${NONE}"
