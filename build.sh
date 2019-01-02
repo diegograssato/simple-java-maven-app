ca6dbe16069b43609da7c08455481d8fc45eb7e4
Diego Pereira Grassato <dgrassato@ciandt.com>
2019-01-02 15:49:21 +0000

    Fix color

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
  
-NONE='\033[0m' # Eliminar as Cores, deixar padrão)
+NONE='\e[0m' # Eliminar as Cores, deixar padrão)
 
 # Regular Colors
-BLACK='\033[1;30m'       # Black
-RED='\033[1;31m'         # Red
-GREEN='\033[1;32m'       # Green
-YELLOW='\033[1;33m'      # Yellow
-BLUE='\033[1;34m'        # Blue
-PURPLE='\033[1;35m'      # Purple
-CYAN='\033[1;36m'        # Cyan
-WHITE='\033[1;37m'       # White
+BLACK='\e[30m'       # Black
+RED='\e[31m'         # Red
+GREEN='\e[32m'       # Green
+YELLOW='\e[33m'      # Yellow
+BLUE='\e[34m'        # Blue
+PURPLE='\e[35m'      # Purple
+CYAN='\e[36m'        # Cyan
+WHITE='\e[37m'       # White
 
 
 #Danger/fail
